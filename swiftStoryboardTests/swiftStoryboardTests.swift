1c5a1f5d995e367f536355580f16b050df84540e
Carolina Castro <144167622+carolCase@users.noreply.github.com>
2024-11-16 13:53:51 +0100

    Initial Commit

---
+++ swiftStoryboardTests/swiftStoryboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  swiftStoryboardTests.swift
+//  swiftStoryboardTests
+//
+//  Created by Carolina Castro on 2024-11-16.
+//
+
+import Testing
+@testable import swiftStoryboard
+
+struct swiftStoryboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
